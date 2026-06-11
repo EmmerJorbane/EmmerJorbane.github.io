bd670ae06bf69fac56f4319136fd03e904531299
EmmerJorbane <sparkleboot@icloud.com>
2026-06-11 15:15:02 +0000

    Reset

--- src/app/components/contact.tsx
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-	return (
-		<section className="contact">
-			<h2>Contact</h2>
-			<p>Contact form will be here.</p>
-		</section>
-	);
-}
--- src/app/components/projects.tsx
+++
@@ -1,8 +0,0 @@
-export default function Projects() {
-	return (
-		<section className="projects">
-			<h2>Projects</h2>
-			<p>Project list coming soon.</p>
-		</section>
-	);
-}
